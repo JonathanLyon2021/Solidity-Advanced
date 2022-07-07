55574de44bacd79f5c251f4dccc9c3a02ccdfa55
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-07-07 18:55:32 -0400

    Initial Commit
    Added Contract SafeMath.

---
+++ Inheritance.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.11;
+
+contract SafeMath {
+
+    function add(int a, int b) internal pure returns (int) {
+        int c = a + b;
+        require(c >= a, "Addition: Interger overflow");
+        return c;
+    }
+
+    function subtract(int a, int b) internal pure returns (int) {
+        require(a >= b, "Subtract: Interger underflow");
+        int c = a - b;
+        return c;
+    }
+
+    function multiply(int a, int b) internal pure returns (int) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+        int c = a * b;
+        require(c / a == b, "Multiplication: Interger overflow");
+        return c;
+    }
+
+}
